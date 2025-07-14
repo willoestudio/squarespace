6673ad0bc2849580e91328e95157e5fd8837c71b
Mik <mikaela.duncan@me.com>
2025-07-14 12:45:09 +1000

    Added code for force height of section

---
+++ wellbeing/force-height.js
@@ -0,0 +1,6 @@
+// FORCE SECTION TO FULL HEIGHT
+
+  document.addEventListener("DOMContentLoaded", () => {
+    const section = document.querySelector('section[data-section-id="681ddc95cfd70541be93a895"]');
+    if (section) section.style.height = window.innerHeight + "px";
+  });
